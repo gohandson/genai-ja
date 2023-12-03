fe11c4c3e07e1676fc91025d4ea21adca67dbac9
Takuya Ueda <uedatakuya@gmail.com>
2023-12-03 10:08:47 +0900

    Add skeleton of section02/step02

---
+++ skeleton/section02/step01/README.md
@@ -0,0 +1,18 @@
+# STEP01: Hello, net/http
+
+## 新しく学ぶこと
+
+* net/httpパッケージ
+
+## 動かし方
+
+```
+$ go build ./genaichat
+$ ./genaichat
+```
+
+または
+
+```
+$ go run ./genaichat
+```
---
+++ skeleton/section02/step01/genaichat/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, net/http")
+}
